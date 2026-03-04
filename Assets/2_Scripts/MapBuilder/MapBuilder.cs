843088e86768ebacb5db21772dba3dc66e960de7
Hyoung-Seok <dkavmdk98@gmail.com>
2026-03-04 23:06:30 +0900

    [MB] 그리드 생성 스크립트 작성

---
+++ Assets/2_Scripts/MapBuilder/MapBuilder.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class MapBuilder : MonoBehaviour
+{
+    [SerializeField] private Vector2Int gridSize;
+    [SerializeField] private GameObject cellPrefab;
+    [SerializeField] private float cellSize = 1f;
+    [SerializeField] private float cellInterval = 0.1f;
+
+    private GameObject _cellObj;
+    
+    private void Start()
+    {
+        CreateGrid();    
+    }
+    
+    public void CreateGrid()
+    {
+        SetCellProperties();
+        
+        var startX = cellSize * ((gridSize.x - 1) * 0.5f);
+        var startZ = cellSize * ((gridSize.y - 1) * 0.5f);
+        
+        var pos = transform.position;
+        var startPos = new Vector3(pos.x - startX, pos.y, pos.z + startZ);
+
+        for (var y = 0; y < gridSize.y; y++)
+        {
+            for (var x = 0; x < gridSize.x; x++)
+            {
+                var cellPos = new Vector3(startPos.x + (x * cellSize), startPos.y, startPos.z - (y * cellSize));
+                var obj = Instantiate(_cellObj, cellPos, Quaternion.identity,  transform);
+                obj.name = $"Cell[{x},{y}]";
+            }
+        }
+        
+        DestroyImmediate(_cellObj);
+    }
+
+    private void SetCellProperties()
+    {
+        _cellObj = Instantiate(cellPrefab, transform);
+
+        var boxCol = _cellObj.GetComponent<BoxCollider>();
+        var model = _cellObj.transform.GetChild(0);
+        var modelSize = cellSize - cellInterval;
+        
+        boxCol.size = new Vector3(cellSize, 0.15f, cellSize);
+        model.localScale = new Vector3(modelSize, 0.15f, modelSize);
+    }
+}
