f52f571089c4588d020df2775aca6e015e82727c
Hyoung-Seok <dkavmdk98@gmail.com>
2026-03-07 01:19:41 +0900

    [MB] 선택한 카테고리에 따라 아이템 불러오는 기능 구현

--- Assets/2_Scripts/MapBuilder/Editor/PaletteEditorWindow.cs
+++ Assets/2_Scripts/MapBuilder/Editor/PaletteEditorWindow.cs
@@ -1,12 +1,16 @@
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
+using System.Collections.Generic;
 
 public class PaletteEditorWindow : EditorWindow
 {
     [SerializeField] private VisualTreeAsset paletteAsset;
+    [SerializeField] private VisualTreeAsset paletteItem;
     
     private VisualElement _root;
+    private DropdownField _categoryDropdown;
+    private VisualElement _assetsContainer;
 
     public static void ShowWindow()
     {
@@ -26,7 +30,47 @@ public class PaletteEditorWindow : EditorWindow
     
     private void InitElements()
     {
-        var categoryDropdown = _root.Q<DropdownField>("ObjCategoryDropdown");
-        categoryDropdown.choices = BuilderAssetLoader.GetAssetCategories();
+        _categoryDropdown = _root.Q<DropdownField>("ObjCategoryDropdown");
+        _assetsContainer = _root.Q<VisualElement>("AssetContainer");
+        
+        _categoryDropdown.choices = BuilderAssetLoader.GetAssetCategories();
+        _categoryDropdown.RegisterValueChangedCallback(CategoryDropdownValueChangedCallback);
+    }
+
+    private void CategoryDropdownValueChangedCallback(ChangeEvent<string> evt)
+    {
+        _assetsContainer.Clear();
+        
+        var paths = BuilderAssetLoader.BuilderAssets[_categoryDropdown.value];
+        var previewTarget = new List<(GameObject, Button)>();
+
+        foreach (var path in paths)
+        {
+            var item = paletteItem.CloneTree();
+            var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+            
+            var assetBtn = item.Q<Button>("AssetButton");
+            item.Q<Label>("AssetName").text = prefab.name;
+            
+            previewTarget.Add((prefab, assetBtn));
+            _assetsContainer.Add(item);
+        }
+
+        rootVisualElement.schedule.Execute(() =>
+        {
+            foreach (var (prefabs, btn) in previewTarget)
+            {
+                var preview = AssetPreview.GetAssetPreview(prefabs);
+
+                if (preview == null)
+                {
+                   continue;
+                }
+                
+                btn.style.width = 128;
+                btn.style.height = 128;
+                btn.style.backgroundImage = preview;
+            }
+        }).Every(200).Until(() => !AssetPreview.IsLoadingAssetPreviews());
     }
 }
