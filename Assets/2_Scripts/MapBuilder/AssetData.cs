a5118618c56701c06ce656f34e593a7723a41e59
Hyoung-Seok <dkavmdk98@gmail.com>
2026-03-09 19:14:29 +0900

    [MB] Asset정보를 저장할 클래스 생성

---
+++ Assets/2_Scripts/MapBuilder/AssetData.cs
@@ -0,0 +1,11 @@
+public class AssetData
+{
+    public string Name;
+    public string Path;
+
+    public AssetData(string name, string path)
+    {
+        Name = name;
+        Path = path;
+    }
+}
