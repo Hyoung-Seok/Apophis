bfc3943a688b46375154d5df751ca39c98704dbf
Hyoung-Seok <dkavmdk98@gmail.com>
2026-03-21 02:18:13 +0900

    [MB] 드랍다운 값이 변경되면 선택된 카테고리의 에셋을 표시하는 로직 구현

--- Assets/2_Scripts/MapBuilder/Editor/PaletteCustomEditor.cs
+++ Assets/2_Scripts/MapBuilder/Editor/PaletteCustomEditor.cs
@@ -6,12 +6,17 @@ using UnityEngine.UIElements;
 
 public class PaletteCustomEditor : EditorWindow
 {
+    public BuilderAssetData CurrentSelectedAsset {get; private set;}
+    
     [SerializeField] private VisualTreeAsset paletteUxml;
+    [SerializeField] private VisualTreeAsset assetsUxml;
 
     private static readonly Vector2Int WINDOW_SIZE = new (1280, 720);
     
     private DropdownField _assetsDropdownField;
     private TextField _assetsPath;
+    private VisualElement _assetsContainer;
+    private VisualElement _favoritesContainer;
 
     public static void ShowWindow()
     {
@@ -33,6 +38,10 @@ public class PaletteCustomEditor : EditorWindow
     {
         _assetsDropdownField = rootVisualElement.Q<DropdownField>("AssetCategory");
         _assetsPath = rootVisualElement.Q<TextField>("AssetPath");
+        _assetsContainer = rootVisualElement.Q<VisualElement>("AssetsContainer");
+        _favoritesContainer = rootVisualElement.Q<VisualElement>("FavoriteContainer");
+
+        _assetsDropdownField.RegisterValueChangedCallback(AddAssetsInContainer);
     }
 
     private void LoadAssets()
@@ -41,5 +50,48 @@ public class PaletteCustomEditor : EditorWindow
         
         MapBuilderAssetLoader.LoadAllAssets(_assetsPath.value);
         _assetsDropdownField.choices = new List<string>(MapBuilderAssetLoader.BuilderAssetData.Keys);
+    }
+
+    private void AddAssetsInContainer(ChangeEvent<string> evt)
+    {
+        _assetsContainer.Clear();
+        
+        var category = _assetsDropdownField.value;
+        var previewList = new List<(TemplateContainer, GameObject)>();
+
+        foreach (var asset in MapBuilderAssetLoader.BuilderAssetData[category])
+        {
+            var root = assetsUxml.CloneTree();
+            var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(asset.Path);
+            
+            root.Q<Label>("AssetName").text = asset.Name;
+            // TODO : 버튼, Fav버튼 클릭 시 동작 로직 구현
+            root.Q<Button>("AssetSelectBtn").clicked += () => OnAssetSelected(asset);
+            
+            previewList.Add((root, prefab));
+            _assetsContainer.Add(root);
+        }
+        
+        rootVisualElement.schedule.Execute(() =>
+        {
+            for (var i = previewList.Count - 1; i >= 0; i--)
+            {
+                var img = AssetPreview.GetAssetPreview(previewList[i].Item2);
+
+                if (img == null)
+                {
+                    continue;
+                }
+                
+                previewList[i].Item1.Q<Button>("AssetSelectBtn").style.backgroundImage = img;
+                previewList.RemoveAt(i);
+            }
+            
+        }).Until(() => previewList.Count == 0).Every(100);
+    }
+
+    private void OnAssetSelected(BuilderAssetData asset)
+    {
+        CurrentSelectedAsset = asset;
     }
 }
