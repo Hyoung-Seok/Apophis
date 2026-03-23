84ce048dfc00375167f347225ebaf87d01f969ba
Hyoung-Seok <dkavmdk98@gmail.com>
2026-03-23 23:53:28 +0900

    [MB] 에셋에 GUID도 저장하도록 변경

--- Assets/2_Scripts/MapBuilder/BuilderAssetData.cs
+++ Assets/2_Scripts/MapBuilder/BuilderAssetData.cs
@@ -4,10 +4,12 @@ public class BuilderAssetData
 {
     public string Path { get; }
     public string Name { get; }
+    public string Guid { get; }
     
-    public BuilderAssetData(string path, string name)
+    public BuilderAssetData(string path, string name, string guid)
     {
         Path = path;
         Name = name;
+        Guid = guid;
     }
 }
--- Assets/2_Scripts/MapBuilder/Editor/MapBuilderAssetLoader.cs
+++ Assets/2_Scripts/MapBuilder/Editor/MapBuilderAssetLoader.cs
@@ -26,7 +26,7 @@ public static class MapBuilderAssetLoader
                 var assetPath = AssetDatabase.GUIDToAssetPath(guid);
                 var assetName = Path.GetFileNameWithoutExtension(assetPath);
                 
-                var assetData = new BuilderAssetData(assetPath, assetName);
+                var assetData = new BuilderAssetData(assetPath, assetName, guid);
                 _builderAssets[category].Add(assetData);
             }
         }
