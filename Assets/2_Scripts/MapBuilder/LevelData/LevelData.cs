acfff07b7f40f6757ee8a97ddca6ed2b9b7bab0f
Hyoung-Seok <dkavmdk98@gmail.com>
2026-04-03 18:48:53 +0900

    [MB] Cell의 종속되지 않는 오브젝트를 저장하기 위한 클래스 생성

--- Assets/2_Scripts/MapBuilder/LevelData/LevelData.cs
+++ Assets/2_Scripts/MapBuilder/LevelData/LevelData.cs
@@ -5,8 +5,10 @@ public class LevelData : ScriptableObject
 {
     public string LevelName => LevelName;
     public List<CellAssetsData> CellAssetData => cellAssetData;
+    public List<FreeAssetsData> FreeAssetData => freeAssetData;
     
     [SerializeField] private string levelName;
     [SerializeField] private List<CellAssetsData> cellAssetData;
+    [SerializeField] private List<FreeAssetsData> freeAssetData;
 
 }
