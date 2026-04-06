98db243fa12c96609c9985c558adb448c2552339
Hyoung-Seok <dkavmdk98@gmail.com>
2026-04-07 03:30:20 +0900

    [MB] 배치한 레벨을 저장하는 기능 구현

---
+++ Assets/2_Scripts/MapBuilder/Editor/LevelDataIO.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEditor;
+
+public static class LevelDataIO
+{
+    private const string DEFAULT_PATH = "Assets/7_Data/MapBuilder/LevelData";
+
+    public static bool Save(MapBuilder mapBuilder, string levelName)
+    {
+        var path = $"{DEFAULT_PATH}/{levelName}.asset";
+        var data = AssetDatabase.LoadAssetAtPath<LevelData>(path);
+
+        if (Exists(path) == true)
+        {
+            if (EditorUtility.DisplayDialog(
+                    "덮어쓰기 확인", $"'{levelName}'이 이미 존재합니다. 덮어쓰시겠습니까?",
+                    "덮어쓰기", "취소") == false)
+            {
+                return false;
+            }
+        }
+        else
+        {
+            data = ScriptableObject.CreateInstance<LevelData>();
+            AssetDatabase.CreateAsset(data, path);
+        }
+        
+        data.SetData(mapBuilder, levelName);
+        EditorUtility.SetDirty(data);
+        AssetDatabase.SaveAssets();
+
+        return true;
+    }
+
+    private static bool Exists(string path) => AssetDatabase.LoadAssetAtPath<LevelData>(path) != null;
+}
