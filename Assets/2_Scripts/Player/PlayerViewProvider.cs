de1f0c9d02306b7b554cbcfba0f004856adeb377
Hyoung-Seok <dkavmdk98@gmail.com>
2026-05-19 22:29:13 +0900

    [P] 환경 시야 처리 구현

--- Assets/2_Scripts/Player/PlayerViewProvider.cs
+++ Assets/2_Scripts/Player/PlayerViewProvider.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 public class PlayerViewProvider : MonoBehaviour
 {
     [SerializeField] private float viewRadius = 3f;
+    [SerializeField] private float viewConeAngle = 60f;
+    [SerializeField] private float viewConeRange = 8f;
     
     private static readonly int PlayerPosID =  Shader.PropertyToID("_PlayerPos");
+    private static readonly int PlayerForwardID = Shader.PropertyToID("_PlayerForward");
     private static readonly int ViewRadiusID = Shader.PropertyToID("_ViewRadius");
+    private static readonly int ConeAngleID = Shader.PropertyToID("_ViewConeAngle");
+    private static readonly int ConeRange = Shader.PropertyToID("_ViewConeRange");
 
     private void LateUpdate()
     {
         Shader.SetGlobalVector(PlayerPosID, transform.position);
+        Shader.SetGlobalVector(PlayerForwardID, transform.forward);
+        
         Shader.SetGlobalFloat(ViewRadiusID, viewRadius);
+        Shader.SetGlobalFloat(ConeAngleID, viewConeAngle);
+        Shader.SetGlobalFloat(ConeRange, viewConeRange);
     }
+
+    #if UNITY_EDITOR
+    private void OnApplicationQuit()
+    {
+        Shader.SetGlobalVector(PlayerPosID, Vector3.zero);
+        Shader.SetGlobalVector(PlayerForwardID, Vector3.zero);
+        
+        Shader.SetGlobalFloat(ViewRadiusID, viewRadius);
+        Shader.SetGlobalFloat(ConeAngleID, viewConeAngle);
+        Shader.SetGlobalFloat(ConeRange, viewConeRange);
+    }
+    #endif
 }
